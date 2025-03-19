f240aee8c958d95ecccd4905cf6e5206c2e0d9c3
AO™ <anthony@osawere.com>
2025-03-19 15:32:13 +0100

    Create layout.php

---
+++ fio/layout.php
@@ -0,0 +1,55 @@
+<?php //*** oLayout - ... » Fio™ © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Fio;
+
+class oLayout
+{
+	// • === slice »
+	public static function slice($file)
+	{
+		return oInc::slice($file);
+	}
+
+
+
+	// • === head »
+	public static function head($file = 'head')
+	{
+		return oInc::slice($file);
+	}
+
+
+
+	// • === header »
+	public static function header($file = 'header')
+	{
+		return oInc::slice($file);
+	}
+
+
+
+
+
+	// • === nav »
+	public static function nav($file = 'navigation')
+	{
+		if ($file === 'navigation') {
+			$file = oInc::slice($file);
+		} else {
+			$file = oInc::slice('nav' . Fio::property('DS') . $file);
+		}
+		return $file;
+	}
+
+
+
+	// • === footer »
+	public static function footer($file = 'footer', $vars = null)
+	{
+		// // if (is_string($vars)) {
+		// // 	$vars = compact($vars);
+		// // }
+		// Fio::dump($vars);
+		return oInc::slice($file, $vars);
+	}
+}
