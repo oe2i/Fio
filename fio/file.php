278bd4dc881f1e62450e5288bf6b4fa6ed4952ae
AO™ <anthony@osawere.com>
2025-03-19 11:23:14 +0100

    Create file.php

---
+++ fio/file.php
@@ -0,0 +1,27 @@
+<?php //*** oFile - ... » Fio™ © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Fio;
+
+class oFile
+{
+	// • property
+	private static bool $init = false;
+
+
+	// • === path »
+	public static function path($file, $real = false)
+	{
+		if ($real === true) {
+			return realpath($file);
+		}
+	}
+
+
+	// • === forbidden »
+	public static function forbidden($file)
+	{
+		$file = self::path($file, true);
+		$script = self::path($_SERVER['SCRIPT_FILENAME'], true);
+		return $file === $script;
+	}
+}
