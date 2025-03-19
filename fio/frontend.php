c1890893b6f9f770a3db427b366cc86892f93405
AO™ <anthony@osawere.com>
2025-03-19 15:32:24 +0100

    Create frontend.php

---
+++ fio/frontend.php
@@ -0,0 +1,41 @@
+<?php //*** oFrontend - ... » Fio™ © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Fio;
+
+class oFrontend
+{
+
+	// • === title »
+	public static function title(?string $title = null)
+	{
+		return '<title>' . Fio::site('firm') . (!empty($title) ? ' - ' . $title : '') . '</title>';
+	}
+
+
+
+	// • === image »
+	public static function image(string $image)
+	{
+		return '<img src="' . Fio::asset()->image($image) . '">';
+	}
+
+
+
+	// • === icon »
+	public static function icon(string $image)
+	{
+		return '<img src="' . Fio::asset()->icon($image) . '">';
+	}
+
+
+
+	// • === copyright »
+	public static function copyright($reserved = true)
+	{
+		$o = "&copy; " . '<strong>' . date('Y') . ' ' . Fio::site('firm') . '</strong>';
+		if ($reserved === true) {
+			$o .= ' All Rights Reserved.';
+		}
+		return $o;
+	}
+}
