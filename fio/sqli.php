3c771a4f71ab139c036bfe82047127119429b904
AO™ <anthony@osawere.com>
2025-03-22 11:03:25 +0100

    Create sqli.php

---
+++ fio/sqli.php
@@ -0,0 +1,38 @@
+<?php //*** oSQLi - ... » Fio™ © 2025 ∞ OE2i™ • www.oe2i.com ∞ Apache License ***//
+
+namespace Fio;
+
+use mysqli;
+
+class oSQLi
+{
+	// • property
+	protected static $mysqli;
+
+
+
+	// • === tryCatch »
+	private static function tryCatch(callable $callback, $default = null, $caller = 'SQLi')
+	{
+		return Fio::tryCatch($callback, $default, $caller);
+	}
+
+
+
+	// • === error »
+	private static function error($message, $extra = null)
+	{
+		return Fio::kill(['title' => 'SQLi', 'message' => $message], $extra);
+	}
+
+
+
+	// • === connect »
+	public static function connect()
+	{
+		$dba = Fio::property('database');
+		self::$mysqli = self::tryCatch(fn() => new mysqli($dba->host, $dba->username, $dba->password, $dba->database));
+		self::$mysqli->set_charset("utf8mb4");
+		return self::$mysqli;
+	}
+}
